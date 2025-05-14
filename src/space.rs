ce2d1a305222541fdc1041450d23c0baa56a6570
Dietrich Daroch <Dietrich@Daroch.me>
2025-05-14 15:50:02 -0400

    Add Search docs and reverse-search draft

--- src/space.rs
+++ src/space.rs
@@ -194,6 +194,27 @@ where
     }
 }
 
+/// A space that allows computing paths in reverse.
+pub trait ReversibleSpace<St, A, C>: Space<St, A, C> + Clone + std::fmt::Debug
+where
+    St: State,
+    A: Action,
+    C: Cost,
+{
+    /// Reverse action
+    #[must_use]
+    fn reverse(&self, a: &A) -> A;
+
+    /// States that can reach a certain state.
+    #[must_use]
+    fn reverse_neighbours(&self, s: &St) -> Vec<(St, A)>;
+
+    /// Some of the States that can reach a certain state.
+    #[must_use]
+    #[cfg(feature = "partial_reverse")]
+    fn partial_reverse_neighbours(&self, s: &St) -> Vec<(St, A)>;
+}
+
 // TODO: Figure out how to use a general heuristic. Do we require sub-goaling?
 /// A general heuristic useful on any problem instance.
 pub trait Heuristic<Sp, St, A, C>: std::fmt::Debug
