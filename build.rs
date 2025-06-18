1a6cb79e5c859f9fafd7666e7c7b91f0ea1a71ce
Dietrich Daroch <Dietrich@Daroch.me>
2025-06-17 20:23:01 -0400

    Build: Try to avoid re-compiles

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use shadow_rs::ShadowBuilder;
 
 fn main() -> std::io::Result<()> {
     ShadowBuilder::builder()
-        .build_pattern(BuildPattern::RealTime)
+        .build_pattern(BuildPattern::Lazy)
         .deny_const(Default::default())
         .build()
         .unwrap();
